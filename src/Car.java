d6e4b9fb2f5efea5cb68a0c13586de9288fc3434
Reinier <rf.schmiermann@student.tue.nl>
2018-04-21 13:44:31 +0200

    Added inCache function

--- src/Car.java
+++ src/Car.java
@@ -2,9 +2,13 @@ import java.util.*;
 public class Car {
     int id;
     ArrayList<Point> locations = new ArrayList<>();
-    ArrayList<Ad> cache = new ArrayList<>();
+    HashSet<Ad> cache = new HashSet<>();
     Ad lastDisplayed = null;
     Car(int id) {
         this.id = id;
+    }
+
+    boolean inCache(Ad a) {
+        return cache.contains(a);
     }
 }
