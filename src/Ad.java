6b3d0fa1426072344bddb96b31acccd416191d73
Vogeltak <vogeltak@gmail.com>
2018-04-21 13:33:05 +0200

    Complete input reading and representation of entities

--- src/Ad.java
+++ src/Ad.java
@@ -1,2 +1,16 @@
 public class Ad {
+    int id;
+    int P;
+    int B;
+    int R;
+    int x, y;
+
+    public Ad(int id, int P, int B, int R, int x, int y) {
+        this.id = id;
+        this.P = P;
+        this.B = B;
+        this.R = R;
+        this.x = x;
+        this.y = y;
+    }
 }
