057812d6003a66c55a5dc37f929545d726e5053f
Vogeltak <vogeltak@gmail.com>
2018-04-21 14:06:32 +0200

    Adds functionality to Car class; to deal with cache

--- src/Car.java
+++ src/Car.java
@@ -3,18 +3,37 @@ public class Car {
     int id;
     ArrayList<Point> locations = new ArrayList<>();
     HashSet<Ad> cache = new HashSet<>();
+    int cacheLimit = Input.M;
     Ad lastDisplayed = null;
+    Ad curAd = null;
+
     Car(int id) {
         this.id = id;
+    }
+
+    void setAdd(Ad a) {
+        if (!inCache(a)) {
+            addCache(a);
+        }
+        curAd = a;
     }
 
     boolean inCache(Ad a) {
         return cache.contains(a);
     }
 
-    int addCache(Ad a) {
-        if (cache.contains(a)) {
+    void addCache(Ad a) {
+        if (cache.size() == cacheLimit) {
+            removeFromCache();
+        }
+        cache.add(a);
+    }
 
+    void removeFromCache() {
+        if (!cache.isEmpty()) {
+            cache.remove(cache.iterator().next());
         }
     }
+
+    void removeFromCache(Ad a) {}
 }
